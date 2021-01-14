3f4263f0b27698948643deb7057cab6df065c835
Umair Khan <me@umair.uk>
2021-01-14 07:31:31 +0000

    Creating ASG using terraform

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,3 +1,5 @@
 region = "us-east-1"
 
 release_bucket_name = "wipro-release-uk"
+
+my_key_pair = "deployer-key"
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type = string
+}
+
+variable "release_bucket_name" {
+  type = string
+}
+
+variable "my_key_pair" {
+    type = string
+}
